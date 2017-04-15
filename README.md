a369c0ca413bfa366c70b1fa8ec9b5f04bfff36f
zccz14 <zccz14@outlook.com>
2017-04-15 13:20:56 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Visual Protocol Buffer Server
+
+Related to [Visual Protocol Buffer](https://github.com/funcxy/visual-protobuf).
+
+# Get started
+
+```
+$ npm install
+$ npm start
+```
+
+Run the server on `localhost:4000`.
---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const spawn = require('child_process').spawn;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+const zlib = require('zlib');
+const archiver = require('archiver');
+
+const app = express();
+// ALlow Origin
+app.use(cors({
+    origin: [
+        'http://localhost:3000'
+    ],
+    credentials: true
+}));
+
+app.use(bodyParser.json());
+
+app.use((req, res) => {
+    fs.mkdtemp(path.join(__dirname, 'worker/'), function (err, folder) {
+        console.log(folder);
+        fs.writeFile(path.join(folder, 'main.proto'), req.body.body, function (err) {
+            if (!err) {
+                const p = spawn('protoc', ['main.proto', '--js_out=.', '--cpp_out=.', '--java_out=.'], {
+                    cwd: folder
+                });
+                p.stdout.on('data', (data) => {
+                    console.log(`stdout: ${data}`);
+                });
+
+                p.stderr.on('data', (data) => {
+                    console.log(`stderr: ${data}`);
+                });
+
+                p.on('close', (code) => {
+                    if (code === 0) {
+                        res.setHeader('Content-Type', 'application/zip');
+                        res.setHeader('Content-disposition', 'attachment; filename=target.zip');
+                        console.log('compile succuess');
+                        const output = fs.createWriteStream(path.join(folder, 'target.zip'));
+                        const archive = archiver('zip');
+
+                        output.on('close', function () {
+                            console.log(archive.pointer() + ' total bytes');
+                            console.log('archiver has been finalized and the output file descriptor has closed.');
+                            // res.sendFile(path.join(folder, 'target.zip'), {}, function(err){
+                            //     if (err) {
+                            //         throw err;
+                            //     }
+                            //     console.log('sent');
+                            // });
+                        });
+
+                        archive.on('error', function (err) {
+                            throw err;
+                        });
+
+                        archive.pipe(res);
+                        archive.bulk([
+                            {
+                                expand: true,
+                                cwd: folder,
+                                src: ['**/*.java', '**/*.cc', '**/*.h', "**/*.js"],
+                                dest: 'source',
+                                dot: true
+                            }
+                        ]);
+                        archive.finalize();
+
+                    }
+                    console.log(`child process exited with code ${code}`);
+                });
+            }
+        })
+    });
+})
+
+app.listen(4000);
